538d403b8fbf968077b381a0991b67aa54baefa0
David Schenck <zero2cx@gmail.com>
2018-01-14 14:07:33 -0600

    fix typo in documentation

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -38,7 +38,7 @@ class SpiralMatrix():
     counter-clockwise manner.
 
     The default style for the generated matrix consists of the series of
-    integers that begin with 1 and then increment by 1 for each member of the
+    integers that begin with 1 and then increments by 1 for each member of the
     series. Either of these integer values may be modified in order to change
     the generated matrix's cell contents. Any positive or negative integer, or
     zero, is acceptable to occupy the center cell that begins the spiral. The
