8597bfcc832100c26fa844f4e5f61b64bc1eb8af
David Schenck <zero2cx@gmail.com>
2018-01-14 19:26:08 -0600

    change version to alpha1; add many config vars

--- setup.py
+++ setup.py
@@ -2,18 +2,34 @@
 # vim: set fileencoding=utf-8 :
 # setup.py
 
+from os import path
 from setuptools import setup, find_packages
+
+def readme(filename):
+    with open(path.join(path.dirname(__file__), filename)) as file:
+        README = file.read()
+    return README
 
 setup(
     name = 'SpiralMatrix',
-    version = '0.1',
+    version = '0.1-a1',
     packages = find_packages(),
-    description = 'Construct a square 2-d matrix with an outward-spiraling '
-            'series of elements.',
+    description = 'Generate a square 2-d matrix with an outward-spiraling '
+            'series of elements',
+    long_description = readme('README.rst')
     author = 'David Schenck',
     author_email = 'zero2cx@gmail.com',
+    license='GPL3+',
     url = 'https://github.com/zero2cx/spiral-matrix',
     download_url = 'https://github.com/zero2cx/spiral-matrix/archive/0.1.tar.gz',
+    include_package_data = True,
+    python_requires = '>=3',
     keywords = ['spiral-matrix', '2d-matrix', 'matrix', 'command-line'],
-    classifiers = [],
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha'
+    ],
 )
