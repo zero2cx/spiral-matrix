e356f511bb8d00e60e27df28bff72625170bc38c
David Schenck <zero2cx@gmail.com>
2018-01-16 23:41:39 -0600

    add current directory to sys.path

---
+++ spiral_matrix/__init__.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# vim: set ff=unix fenc=utf-8 et ts=4 sts=4 sta sw=4:
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+if __name__ == '__main__':
+    pass
