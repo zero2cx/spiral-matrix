9d1a0a88c44cbbf4d7d77468e90a9424688fb6ee
David Schenck <zero2cx@gmail.com>
2018-01-17 21:19:18 -0600

    update package version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(filename):
 
 setup(
     name = 'spiral-matrix',
-    version = '0.1.2a5',
+    version = '0.1.3a1',
     packages = find_packages(),
     description = 'Generate a square 2-d matrix with an outward-spiraling '
             'series of elements',
@@ -26,7 +26,6 @@ setup(
     author_email = 'zero2cx@gmail.com',
     license='GPL3+',
     url = 'https://github.com/zero2cx/spiral-matrix',
-    # download_url = 'https://github.com/zero2cx/spiral-matrix/archive/v0.1.2a5.tar.gz',
     include_package_data = True,
     python_requires = '>=3',
     keywords = [
