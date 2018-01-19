112decb5db300a3eda9482890aff66735ecade7b
David Schenck <zero2cx@gmail.com>
2018-01-19 02:18:53 -0600

    remove try/except block in two functions

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -201,10 +201,7 @@ class SpiralMatrix():
 
         y, x = coords
 
-        try:
-            self.matrix[y][x] = self.series[index]
-        except IndexError:
-            return index + 1
+        self.matrix[y][x] = self.series[index]
 
         return index + 1
 
@@ -216,14 +213,8 @@ class SpiralMatrix():
         '''
 
         vector = self.vector[look][bearing]
-        y, x = [sum(coords) for coords in zip(location, vector)]
 
-        try:
-            self.matrix[y][x]
-        except IndexError:
-            raise AttributeError
-
-        return y, x
+        return [sum(coords) for coords in zip(location, vector)]
 
     def _turn(self, turn, bearing):
         '''
